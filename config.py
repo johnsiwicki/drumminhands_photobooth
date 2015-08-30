ca5773cdab0aa8036638f02e98f7fb71bc372f47
John Siwicki <johnsiwicki@gmail.com>
2015-08-30 16:57:12 -0400

    Updated py file with new tumbler

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # Copy this file to config.py and replace the values with your information
 # OAuth keys can be generated from https://api.tumblr.com/console/calls/user/info
-consumer_key='CONSUMER_KEY' #replace with your key
-consumer_secret='CONSUMER_SECRET' #replace with your secret code
-oath_token='OATH_TOKEN' #replace with you oath token
-oath_secret='OATH_SECRET' #replace with your oath secret code
-tumblr_blog = 'TUMBLR_BLOG' # replace with your tumblr account name without .tumblr.com
-file_path = '/home/pi/photobooth/pics/' # path to save images
+consumer_key='nlI5Wy0IZGOXqnh2uImDhDq1ykMtC0szYxGgkxije0GaSl24PN' #replace with your key
+consumer_secret='YlszMjQpOec4mVDey8nH6sY6UGNCsQKerO8ADN4zapk0ZAbUiM' #replace with your secret code
+oath_token='nlI5Wy0IZGOXqnh2uImDhDq1ykMtC0szYxGgkxije0GaSl24PN' #replace with you oath token
+oath_secret='YlszMjQpOec4mVDey8nH6sY6UGNCsQKerO8ADN4zapk0ZAbUiM' #replace with your oath secret code
+tumblr_blog = 'Photo Booth' # replace with your tumblr account name without .tumblr.com
+file_path = '/home/pi/photobooth/' # path to save images
